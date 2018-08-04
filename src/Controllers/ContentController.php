25f7fcfeb663e0d9d66f73f68288082f087c186c
Jost Hanken <jost.hanken@plentymarkets.com>
2018-08-04 16:51:45 +0200

    update controller again

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -10,6 +10,6 @@ class ContentController extends Controller
 {
     public function sayHello(Twig $twig):string
     {
-        return $twig->render('WidgetPlugin::hello');
+        return $twig->render('WidgetPlugin::content.hello');
     }
 }
