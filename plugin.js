c09dea01744c8d5ac0fa697d74a7edd221060bb6
Jost Hanken <jost.hanken@plentymarkets.com>
2018-08-04 14:09:23 +0200

    plugin.js => plugin.json

--- plugin.js
+++
@@ -1,8 +0,0 @@
-{
-    "name"              :"Widget - Google Website-Inhaberschaft bestätigen",
-    "description"       :"Google Website-Inhaberschaft bestätigen",
-    "namespace"         :"WidgetPlugin\\Providers\\GoogleSiteVerificationServiceProvider",
-    "author"            :"Jost Robert Hanken",
-    "type"              :"template",
-    "serviceProvider"   :null
-}
