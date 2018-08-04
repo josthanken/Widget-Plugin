3607d96143759c6a807fb1ac95b180240ae574de
Jost Hanken <jost.hanken@plentymarkets.com>
2018-08-04 13:57:06 +0200

    grundstein

---
+++ src/GoogleSiteVerificationServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace WidgetPlugin\Providers;
+
+
+use Plenty\Plugin\ServiceProvider;
+
+class GoogleSiteVerificationServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     */
+
+    public function register()
+    {
+
+    }
+}
+
+public function register()
+{
+    $this->getApplication()->register(GoogleSiteVerificationRouteServiceProvider::class);
+}
+
+public function map(Router $router)
+{
+    $router->get('hello','WidgetPlugin\Controllers\ContentController@sayHello');
+}
---
+++ src/Providers/GoogleSiteVerificationRouteServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace WidgetPlugin\Providers;
+
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+class GoogleSiteVerificationRouteServiceProvider extends RouteServiceProvider
+{
+    public function map(Router $router)
+    {
+
+    }
+}
+
+public function map(Router $router)
+{
+    $router->get('hello','WidgetPlugin\Controllers\ContentController@sayHello');
+}
