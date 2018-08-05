a39988d52f175911a2c262d58dd92a046da09bd4
Jost Hanken <jost.hanken@plentymarkets.com>
2018-08-05 12:47:48 +0200

    More Data changes

--- src/Widgets/Common/GoogleSiteVerification.php
+++ src/Widgets/Common/GoogleSiteVerification.php
@@ -1,13 +1,10 @@
-{% set text = widget.settings.text.mobile %}
+<?php
 
-<div class="widget widget-text widget-{{ widget.settings.appearance.mobile | default('primary') }}">
-    <div class="widget-inner">
-        {% if text | trim is not empty %}
-        {{ text | raw }}
-        {% else %}
-        <div class="placeholder-text">
-            {{ trans("Ceres::Widget.textPlaceholder") }}
-        </div>
-        {% endif %}
-    </div>
-</div>
+namespace Ceres\Widgets\Common;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class GoogleSiteVerification extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.Common.GoogleSiteVerification";
+}
